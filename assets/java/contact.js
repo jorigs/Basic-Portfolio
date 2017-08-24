3ba4152a4a92b4dd7088a22578a385cf03cbd3a4
Jordan <jordan.a.rigsby@gmail.com>
2017-08-24 12:33:59 -0400

    javascript added, minor formatting tweaks

--- assets/java/contact.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function()
-{
-  var rld_cnt=parseInt($.cookie('rld_cnt')||0)+1;
-  $.cookie('rld_cnt',rld_cnt);
-  var text = ["Good Morning","Good Night"];
-  $('#subject').html(text[rld_cnt%2]);
-
-});
-
